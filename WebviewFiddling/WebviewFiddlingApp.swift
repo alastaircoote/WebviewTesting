7029bccaf9e5c844d8f1dc3738587e588ec1cf7a
Alastair Coote <alastair.coote@nytimes.com>
2023-03-28 16:08:21 -0400

    Initial Commit

---
+++ WebviewFiddling/WebviewFiddlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewFiddlingApp.swift
+//  WebviewFiddling
+//
+//  Created by Alastair on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WebviewFiddlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
